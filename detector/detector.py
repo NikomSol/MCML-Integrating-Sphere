3e30f5ab8e991346b7d035a4810fbd21f9330060
Nikita <67433712+NikomSol@users.noreply.github.com>
2023-06-03 18:21:23 +0300

    add Detector with ALL mode, start DirectProblem

---
+++ detector/detector.py
@@ -0,0 +1,83 @@
+import numpy as np
+from numba import njit
+
+
+from .cfg import DetectorCfg
+from .measurement import Measurement
+from .probe import Probe
+
+
+class Detector:
+    def __init__(self, cfg: DetectorCfg):
+        self.cfg = cfg
+        self.get_func_save_progress,
+        self.get_func_save_ending,
+        self.get_func_get_storage = self.parse_cfg()
+
+    def parse_cfg(self):
+        cfg = self.cfg
+        probe = cfg.probe
+        measurment = cfg.measurement
+
+        if measurment is Measurement.ALL:
+            return self.get_func_save_progress_ALL, self.get_func_save_ending_ALL, self.get_func_get_storage_ALL
+        else:
+            raise NotImplementedError()
+
+    # ALL variant
+    def get_func_save_progress_ALL(self):
+
+        @njit(fastmath=True)
+        def save_progress(p_gen, p_move, p_term, p_turn, storage):
+            # TODO does supression unused variable increase the calculation time? 
+            _storage = storage * 1.
+            _, _, _, _ = p_gen, p_move, p_term, p_turn
+            return _storage
+
+        return save_progress
+
+    def get_func_save_ending_ALL(self):
+
+        @njit(fastmath=True)
+        def save_ending(p_gen, p_move, p_term, p_turn, storage):
+            _storage = storage * 1.
+            if p_move in [-1, -2]:
+                _storage[0] += p_move[2, 0]
+                _storage[1] += p_gen[2, 0] - p_move[2, 0]
+            if p_term == 0:
+                _storage[1] += p_gen[2, 0]
+            return _storage
+
+        return save_ending
+
+    def get_func_get_storage_ALL(self):
+
+        @njit(fastmath=True)
+        def get_storage():
+            return np.zeros(2)
+
+        return get_storage
+
+    # def get_func_save_progress(self):
+
+    #     @njit(fastmath=True)
+    #     def save_progress():
+    #         raise NotImplementedError()
+
+    #     return save_progress
+
+    # def get_func_save_ending(self):
+
+    #     @njit(fastmath=True)
+    #     def save_ending():
+    #         raise NotImplementedError()
+
+    #     return save_ending
+
+    # def get_func_get_storage(self):
+
+    #     @njit(fastmath=True)
+    #     def get_storage():
+    #         raise NotImplementedError()
+
+    #     return get_storage
--- direct_problem/direct_problem.py
+++ direct_problem/direct_problem.py
@@ -1,42 +1,72 @@
-# import numpy as np
-# from numba import njit
+import numpy as np
+from numba import njit
+
+from sample import Sample
+from source import Source
+from detector import Detector
+from .cfg import DirectProblemCfg
 
 
-# class DirectProblem:
-#     def __init__(self, cfg, sample, source, detector):
-#         self.cfg = cfg
-#         self.sample = sample
-#         self.source = source
-#         self.detector = detector
+class DirectProblem:
+    def __init__(self, cfg: DirectProblemCfg,
+                 sample: Sample, source: Source, detector: Detector):
+        self.cfg = cfg
+        self.sample = sample
+        self.source = source
+        self.detector = detector
 
-#         self.trace = self._get_trace()
+        self.trace = self._get_trace()
 
-#     def _get_trace(self):
-#         source = self.source
-#         detector = self.detector
+    def _get_trace(self):
+        source = self.source
+        detector = self.detector
 
-#         generator = source.func_generator
+        generator = source.get_func_generator
 
-#         save_progress = detector.func_save_progress
-#         save_ending = detector.func_save_ending
-#         storage = detector.new_storage()
+        save_progress = detector.get_func_save_progress
+        save_ending = detector.get_func_save_ending
+        storage = detector.get_func_get_storage
 
-#         move = self.get_func_move()
-#         term = self.get_func_term()
-#         turn = self.get_func_turn()
+        move = self.get_func_move()
+        term = self.get_func_term()
+        turn = self.get_func_turn()
 
-#         @njit(fastmath=True)
-#         def trace():
-#             raise NotImplementedError()
+        @njit(fastmath=True)
+        def trace():
+            raise NotImplementedError()
 
-#         return trace
+        return trace
 
-#     def solve(self):
-#         N = self.cfg.N
-#         trace = self.trace
-#         storage = self.detector.get_storage()
+    def solve(self):
+        N = self.cfg.N
+        trace = self.trace
+        storage = self.detector.get_storage()
 
-#         for _ in range(N):
-#             storage += trace()
+        for _ in range(N):
+            storage += trace()
 
-#         return storage
+        return storage
+
+    def get_func_move(self):
+        
+        @njit(fastmath=True)
+        def move():
+            raise NotImplementedError()
+        
+        return move
+
+    def get_func_turn(self):
+        
+        @njit(fastmath=True)
+        def turn():
+            raise NotImplementedError()
+        
+        return turn
+
+    def get_func_term(self):
+        
+        @njit(fastmath=True)
+        def term():
+            raise NotImplementedError()
+        
+        return term
