28011aba9cbe7430dac81b0bf153de9600936c0a
Nikita <67433712+NikomSol@users.noreply.github.com>
2023-06-21 21:58:09 +0300

    plot upd

--- detector/plot.py
+++ detector/plot.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class Plot:
     pass
 
@@ -8,4 +12,12 @@ class PlotIntegratingSphere(Plot):
         self.positions = positions
 
     def plot(self, storage):
+        positions = self.positions
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+        ax1.plot(positions, storage[0])
+        ax1.legend(title='Transmition')
+        ax2.plot(positions, storage[1])
+        ax2.legend(title='Reflection')
+
+    def plot_log(self, storage):
         raise NotImplementedError
